8dbdb7570ecf91bd4e1c13ac0218dbd5ede34b1f
junqiwu02 <junqiwu02@gmail.com>
2018-02-19 12:29:45 -0500

    added switching between cube control and non-cube control

--- src/org/usfirst/frc4930/Kermit/subsystems/Arm.java
+++ src/org/usfirst/frc4930/Kermit/subsystems/Arm.java
@@ -56,10 +56,18 @@ public class Arm extends Subsystem
   public void set(double speed) {
     if (speed > 0.2) {
       updatePosition();
-      extend();
+      if(!Robot.limitSwitch.hasCube) {
+    	  extendWithCube();
+      } else {
+    	  extend();
+      }
     } else if (speed < -0.2) {
       updatePosition();
-      retract();
+      if(!Robot.limitSwitch.hasCube) {
+    	  retractWithCube();
+      } else {
+    	  retract();
+      }
     } else {
 //      System.out.println("Adjusting....");
       adjustElbow(elbowPos);
@@ -144,11 +152,11 @@ public class Arm extends Subsystem
     } else {
       // if they are below the bar, move one at a time
       // if lower arm is not retracted, retract lower arm
-      if (encoder.lShoulder_Raw() > 200) {
+      if (encoder.lShoulder_Raw() > 0) {
     	  System.out.println("RETRACTING SHOULDER");
         lShoulder.set(Constants.SHOULDER_LOWER_SPD_BELOW_BAR);
         lElbow.set(0.05);
-      } else if (encoder.lElbow_Raw() > 200) {
+      } else if (encoder.lElbow_Raw() > 0) {
     	  System.out.println("RETRACTING ELBOW");
         // else if upper arm is not retracted, retract upper arm
         lShoulder.set(0.05);
@@ -158,6 +166,68 @@ public class Arm extends Subsystem
 
     }
   }
+  
+  private void extendWithCube() {
+	    // limit switches return false when pressed
+	    // if the upper arm is not fully extended, extend upper arm
+	    if (encoder.lElbow_Raw() < Constants.ELBOW_TO_BAR) {
+	    	System.out.println("EXTENDING ELBOW");
+	    	// 0.05 will maintain the position
+	    	lShoulder.set(0.05);
+	    	// elbow moves faster than the shoulder
+	    	lElbow.set(Constants.ELBOW_RAISE_SPD_BELOW_BAR_CUBE);
+	    } else if (encoder.lShoulder_Raw() < Constants.SHOULDER_TO_BAR) {
+	    	System.out.println("EXTENDING SHOULDER");
+	    	// else if lower arm is not fully extended, extend lower arm
+	    	lShoulder.set(Constants.SHOULDER_RAISE_SPD_BELOW_BAR_CUBE);
+	    	lElbow.set(0.2);
+	    } else {
+	      System.out.println("EXTENDING BOTH");
+
+	      	// if both arms are past the bar, run each until they reach the limit
+	      	if (encoder.lElbow_Raw() < Constants.ELBOW_EXTENDED) {
+	      		lElbow.set(Constants.ELBOW_RAISE_SPD_ABOVE_BAR_CUBE);
+	      	} else {
+	      		// maintain if at the max
+	      		lElbow.set(0.05);
+	      	}
+
+	      	if (encoder.lShoulder_Raw() < Constants.SHOULDER_EXTENDED) {
+	      		lShoulder.set(Constants.SHOULDER_RAISE_SPD_ABOVE_BAR_CUBE);
+
+	      	} else {
+	      		// maintain if at the max
+	      		lShoulder.set(0.05);
+	      	}
+	    }
+	  }
+
+	  private void retractWithCube() {
+	    // limit switches return false when pressed
+	    // if both are above the bar, move both at once
+	    if (encoder.lShoulder_Raw() > Constants.SHOULDER_TO_BAR
+	        && encoder.lElbow_Raw() > Constants.ELBOW_TO_BAR) {
+	      System.out.println("RETRACTING BOTH");
+	      lShoulder.set(Constants.SHOULDER_LOWER_SPD_ABOVE_BAR_CUBE);
+	      lElbow.set(Constants.ELBOW_LOWER_SPD_ABOVE_BAR_CUBE);
+
+	    } else {
+	      // if they are below the bar, move one at a time
+	      // if lower arm is not retracted, retract lower arm
+	      if (encoder.lShoulder_Raw() > 0) {
+	    	  System.out.println("RETRACTING SHOULDER");
+	        lShoulder.set(Constants.SHOULDER_LOWER_SPD_BELOW_BAR_CUBE);
+	        lElbow.set(0.05);
+	      } else if (encoder.lElbow_Raw() > 0) {
+	    	  System.out.println("RETRACTING ELBOW");
+	        // else if upper arm is not retracted, retract upper arm
+	        lShoulder.set(0.05);
+	        // elbow moves faster than the shoulder
+	        lElbow.set(Constants.ELBOW_LOWER_SPD_BELOW_BAR_CUBE);
+	      }
+
+	    }
+	  }
 
   public void resetEncoders() {
     encoder.ArmReset();
